b356a5ac9dea76c31204eec1ce2d0ebafa453a73
Masha Klimina <kliminaklimina@gmail.com>
2024-03-24 17:49:26 +0000

    test

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+//
+// Created by Мария on 23/03/2024.
+//
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+# define HIGHEST_GRADE 1
+# define LOWEST_GRADE 150
+#include <iostream>
+
+class Bureaucrat {
+
+    private:
+        std::string _name;
+        int         _grade;
+    public:
+        Bureaucrat(); //done
+        Bureaucrat(std::string _name, int grade); //done
+        ~Bureaucrat(); //done
+        Bureaucrat(Bureaucrat const &cls); //done
+        Bureaucrat &operator=(Bureaucrat &cls); //done
+        std::string getName();//done
+        int getGrade(); //done
+        void Increment(); //done
+        void Decrement(); //done
+        class	GradeTooHighException: public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class	GradeTooLowException: public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+    };
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & obj);
+
+#endif //BUREAUCRAT_HPP
