218ab9baefd24647bb00d949a3f147a084048bb9
Mariia KLIMINA <mklimina@paul-f5Ar4s5.clusters.42paris.fr>
2024-05-06 15:22:52 +0200

    yep

--- CPP06/ex00/headers/ScalarConverter.hpp
+++ CPP06/ex00/headers/ScalarConverter.hpp
@@ -3,8 +3,12 @@
 #include <climits>
 #include <cstdio>
 #include <cfloat>
-
 #include <cmath>
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
+const std::string CYAN = "\033[36m";
+const std::string RESET = "\033[0m";
 
 enum s_Type {
 	CHAR,
@@ -23,4 +27,20 @@ class ScalarConverter
 		ScalarConverter &operator=(const ScalarConverter &cls);
 	public:
 		static void convert(std::string &input);	
+		class NonExistentType: public std::exception
+		{
+			public:
+                virtual const char *what() const throw();
+		};
+		class ConversionImpossible: public std::exception
+		{
+			public:
+                virtual const char *what() const throw();
+		};
+		class OutputNotToDisplay: public std::exception
+		{
+			public:
+                virtual const char *what() const throw();
+		};
+
 };
--- CPP06/ex00/src/ScalarConverter.cpp
+++ CPP06/ex00/src/ScalarConverter.cpp
@@ -1,10 +1,13 @@
 
 #include "../headers/ScalarConverter.hpp"
 
-bool isInt(std::string &input)
+bool	isInt(std::string &input)
 {
-	int i = 0;
-	int len = input.length();
+	int	i;
+	int	len;
+
+	i = 0;
+	len = input.length();
 	if (input[i] == '+' || input[i] == '-')
 		i++;
 	if (i == len)
@@ -16,49 +19,53 @@ bool isInt(std::string &input)
 		i++;
 	}
 	if (atoll(input.c_str()) > INT_MAX || atoll(input.c_str()) < INT_MIN)
-		return(false);
-
-	return(true);
-
+		return (false);
+	return (true);
 }
 
-bool isFloat(std::string &input)
+bool	isFloat(std::string &input)
 {
-	int i = 0;
-	int len = input.length();
+	int	i;
+	int	len;
+
+	i = 0;
+	len = input.length();
 	if (input == "inff" || input == "-inff" || input == "nanf")
-		return(true);
+		return (true);
 	if (input[i] == '+' || input[i] == '-')
 		i++;
 	while (i < len && isdigit(input[i]))
 		i++;
 	if (input[i] != '.')
-		return(false);
+		return (false);
 	i++;
 	while (i < len && isdigit(input[i]))
 		i++;
 	if (input[i] != 'f')
-		return(false);
+		return (false);
 	i++;
 	if (i != len)
-		return(false);
-	if (atof(input.c_str()) > FLT_MAX || atof (input.c_str()) < -FLT_MAX)
-		return(false);
-	return(true);
+		return (false);
+	if (atof(input.c_str()) > FLT_MAX || atof(input.c_str()) < -FLT_MAX)
+		return (false);
+	return (true);
 }
 
-bool isDouble(std::string &input)
+bool	isDouble(std::string &input)
 {
-	int i = 0;
-	int len = input.length();
+	int	i;
+	int	len;
+
+	i = 0;
+	len = input.length();
 	if (input == "-inf" || input == "+inf" || input == "nan")
-		return(true);
+		return (true);
 	if (input[i] == '+' || input[i] == '-')
 		i++;
 	while (i < len && isdigit(input[i]))
 		i++;
 	if (input[i] != '.')
-		return(false);
+		return (false);
 	i++;
 	if (i == len)
 		return (false);
@@ -66,38 +73,180 @@ bool isDouble(std::string &input)
 		i++;
 	if (i != len)
 		return (false);
-	if (atof(input.c_str()) > LDBL_MAX || atof (input.c_str()) < -LDBL_MAX)
-		return(false);
-
-	return(true);
-
+	if (atof(input.c_str()) > LDBL_MAX || atof(input.c_str()) < -LDBL_MAX)
+		return (false);
+	return (true);
 }
 
-bool isChar(std::string &input)
+bool	isChar(std::string &input)
 {
-	int i = 0;
-	int len = input.length();
+	int	i;
+	int	len;
+
+	i = 0;
+	len = input.length();
 	if (isprint(input[i]) && len == 1)
-		return(true);
-	return(false);
+		return (true);
+	return (false);
 }
 
-int defineType(std::string &input)
+int	defineType(std::string &input)
 {
 	if (isInt(input))
-		return(INT);
+		return (INT);
 	else if (isFloat(input))
-		return(FLOAT);
+		return (FLOAT);
 	else if (isDouble(input))
-		return(DOUBLE);
+		return (DOUBLE);
 	else if (isChar(input))
-		return(CHAR);
-	else 
-		return(123);	
+		return (CHAR);
+	else
+		throw ScalarConverter::NonExistentType();
+}
+
+int	toInt(std::string &input)
+{
+	double	value;
+
+	if (input == "inf" || input == "-inf" || input == "inff"
+		|| input == "-inff")
+		throw ScalarConverter::ConversionImpossible();
+	if (defineType(input) == CHAR)
+		return (static_cast<int>(input[0]));
+	value = atof(input.c_str());
+	if (value > INT_MAX || value < INT_MIN)
+		throw ScalarConverter::OutputNotToDisplay();
+	return (static_cast<int>(value));
+}
+
+double	toDouble(std::string &input)
+{
+	long double	value;
+
+	if (input == "-inff" || input == "-inf")
+		return (-DBL_MAX * 2);
+	if (input == "+inff" || input == "+inf")
+		return (DBL_MAX * 2);
+	if (defineType(input) == CHAR)
+		return (static_cast<double>(input[0]));
+	value = atof(input.c_str());
+	if (value > FLT_MAX || value < -FLT_MAX)
+		throw ScalarConverter::OutputNotToDisplay();
+	return (static_cast<double>(value));
+}
+
+float	toFloat(std::string &input)
+{
+	double	value;
+
+	if (input == "-inff" || input == "-inf")
+		return (-FLT_MAX * 2);
+	if (input == "+inff" || input == "+inf")
+		return (FLT_MAX * 2);
+	if (defineType(input) == CHAR)
+		return (static_cast<float>(input[0]));
+	value = atof(input.c_str());
+	if (value > FLT_MAX || value < -FLT_MAX)
+		throw ScalarConverter::OutputNotToDisplay();
+	return (static_cast<float>(value));
+}
+
+char	toChar(std::string &input)
+{
+	double	value;
+	char	c;
+
+	if (defineType(input) == CHAR)
+		return (static_cast<float>(input[0]));
+	value = atof(input.c_str());
+	if (value > CHAR_MAX || value < CHAR_MIN)
+		throw ScalarConverter::ConversionImpossible();
+	c = static_cast<char>(value);
+	if (std::isprint(c))
+		return (c);
+	else
+		throw(ScalarConverter::OutputNotToDisplay());
+}
+const std::string whichType(int type)
+{
+	switch (type)
+	{
+	case CHAR:
+		return ("CHAR");
+	case INT:
+		return ("INT");
+	case FLOAT:
+		return ("FLOAT");
+	case DOUBLE:
+		return ("DOUBLE");
+	}
+	return ("NONE");
 }
 
 void ScalarConverter::convert(std::string &input)
 {
-	int out = defineType(input);
-	std::cout << "DEFINED TYPE IS: " << out << std::endl;
+	int	len;
+	int	out;
+
+	len = input.length();
+	if (len == 0)
+	{
+		std::cerr << RED << "Error: Input length can't be zero." << RESET << std::endl;
+		exit(1);
+	}
+	for (int i = 0; i < len; i++)
+	{
+		if (!isprint(input[i]))
+		{
+			std::cerr << RED << "Error: Input contains non-printable characters." << RESET << std::endl;
+			exit(1);
+		}
+	}
+	out = defineType(input);
+	std::cout << YELLOW << "DEFINED TYPE IS: " << whichType(out) << RESET << std::endl;
+	try
+	{
+		std::cout << CYAN << "CONVERT TO CHAR: " << RESET << GREEN << toChar(input) << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << CYAN << "CONVERT TO INT: " << RESET << GREEN << toInt(input) << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << CYAN << "CONVERT TO FLOAT: " << RESET << GREEN << toFloat(input) << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << CYAN << "CONVERT TO DOUBLE: " << RESET << GREEN << toDouble(input) << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << RED << "Error: " << e.what() << RESET << std::endl;
+	}
+}
+
+const char *ScalarConverter::NonExistentType::what(void) const throw()
+{
+	return ("Given type does not exist.");
+}
+const char *ScalarConverter::ConversionImpossible::what(void) const throw()
+{
+	return ("Conversion is impossible. ");
+}
+const char *ScalarConverter::OutputNotToDisplay::what(void) const throw()
+{
+	return ("Output can't be displayed.");
 }
