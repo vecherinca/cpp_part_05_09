c1b103796de249a84551cf98f6c75d9b491e904e
Mariia KLIMINA <mklimina@paul-f5Ar4s4.clusters.42paris.fr>
2024-05-07 14:53:10 +0200

    let'sgo

---
+++ CPP07/ex00/src/Whatever.cpp
@@ -0,0 +1,22 @@
+
+#include "../headers/Whatever.hpp"
+Whatever::Whatever( void ){
+
+	return ;
+}
+
+Whatever::Whatever(const Whatever &sc){
+
+	return ;
+}
+
+Whatever::~Whatever( void ){
+
+	return ;
+}
+
+Whatever &Whatever::operator=(const Whatever &sc){
+
+	return (*this);
+}
+
