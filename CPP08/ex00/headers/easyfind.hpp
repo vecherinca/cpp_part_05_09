28cfbb134f44b66d9d9cc3deff0f0d9a3081d571
Mariia KLIMINA <mklimina@paul-f4Ar1s4.clusters.42paris.fr>
2024-05-09 17:30:55 +0200

    ex00 ok

---
+++ CPP08/ex00/headers/easyfind.hpp
@@ -0,0 +1,20 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+
+#include <algorithm>
+#include <iostream>
+#include <iterator> 	 	 
+#include <stdexcept>
+#include <vector>
+#include <list>
+	
+template<typename T>
+		int easyfind(const T& content, int number) {
+		typename T::const_iterator it = std::find(content.begin(), content.end(), number);
+    	if (it == content.end()) {
+        throw std::runtime_error("Value not found");
+    	}
+    	return static_cast<int>(it - content.begin());
+		}
+#endif
